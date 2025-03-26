639fcedb644b49c38c1a15ebbe7dec500e8ea696
Murgwell <romar.alaman@cit.edu>
2025-03-26 11:34:27 +0800

    bomboclaat

--- app/src/main/java/cit/edu/KlockApp/ui/main/alarm/AlarmViewModel.kt
+++ app/src/main/java/cit/edu/KlockApp/ui/main/alarm/AlarmViewModel.kt
@@ -3,11 +3,18 @@ package cit.edu.KlockApp.ui.main.alarm
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.sql.Date
+import java.sql.Time
 
 class AlarmViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is alarm Fragment"
+    private val _alarms = MutableLiveData<List<Alarm>>().apply {
+        value = listOf(
+            Alarm(Time.valueOf("08:00:00"), true, Date.valueOf("2025-03-26")),
+            Alarm(Time.valueOf("12:00:00"), false, Date.valueOf("2025-03-27")),
+            Alarm(Time.valueOf("18:00:00"), true, Date.valueOf("2025-03-28"))
+        )
     }
-    val text: LiveData<String> = _text
-}
+
+    val alarms: LiveData<List<Alarm>> = _alarms
+    }
